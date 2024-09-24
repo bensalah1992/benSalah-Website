46290c5649961e78161583b0e32704f7ebf250b7
offline <a.bensalah.net@gmail.com>
2024-09-24 14:12:09 +0200

    Fix deployment - config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
-    site: "https://ben-salah.netlify.app"
+export default defineConfig({
+    site: 'https://ben-salah.netlify.app'
+});
